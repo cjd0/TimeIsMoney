de2360bb00956a95784e445675ef301fe099cae7
Chris <chrisdoyle20@gmail.com>
2023-08-26 22:06:21 +0100

    Hourly wage calculation
    - Added some code to the Settings Activity to allow the user to calculate their hourly wage based on annual salary and hours worked per week.
    
    - Included NumberFormat in the wage calculation so output can be treated as a currency with formatting dependent on the language setting of the user's device.
    
    - Updated compileSDK and targetSdk to 33 and minSdk to 24 in gradle.build (module)

--- app/src/main/java/com/chrisdoyle/timeismoney/MainActivity.java
+++ app/src/main/java/com/chrisdoyle/timeismoney/MainActivity.java
@@ -2,13 +2,32 @@ package com.chrisdoyle.timeismoney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnGoToSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGoToSettings = (Button)findViewById(R.id.btnGoToSettings);
+
+        btnGoToSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenSettings();
+            }
+        });
+    }
+
+    public void OpenSettings() {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/chrisdoyle/timeismoney/Settings.java
+++ app/src/main/java/com/chrisdoyle/timeismoney/Settings.java
@@ -2,13 +2,51 @@ package com.chrisdoyle.timeismoney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
+
+    private EditText etInputSalary, etInputHoursWorked;
+    private TextView tvDisplayHourly;
+    private Button btnCalculateHourly;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //assign UI
+        etInputSalary = (EditText)findViewById(R.id.etSettingsSetAnnualSalary);
+        etInputHoursWorked = (EditText)findViewById(R.id.etSettingsSetWeeklyHours);
+        tvDisplayHourly = (TextView)findViewById(R.id.tvSettingsAnnualSalaryDisplay);
+        btnCalculateHourly = (Button)findViewById(R.id.btnSettingsCalculateHourly);
+
+        //set button actions
+        btnCalculateHourly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CalculateHourly();
+            }
+        });
     }
+
+
+    //methods
+    public void CalculateHourly() {
+
+        if (etInputSalary.getText().toString().length() != 0 && etInputHoursWorked.getText().toString().length() != 0) {
+            double hourly = (Double.parseDouble(etInputSalary.getText().toString()) / 52) / Double.parseDouble(etInputHoursWorked.getText().toString());
+            NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+            tvDisplayHourly.setText("Hourly wage: " + currencyFormat.format(hourly));
+        }
+        else {
+            tvDisplayHourly.setText("Please input Annual Salary and Weekly Hours.");
+        }
+    }
+
 }
